087b3f3bccc39b1fed5f401d3af23af10400de66
Adam Snyder <armsnyder@gmail.com>
2024-06-23 15:38:28 -0700

    chore: add release automation

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -10,15 +13,75 @@ import (
 )
 
 func main() {
-	log.SetFlags(0)
+	// Parse command line flags.
+
+	var args struct {
+		version  bool
+		help     bool
+		testdata string
+	}
+
+	flag.BoolVar(&args.version, "version", false, "Print the version and exit")
+	flag.BoolVar(&args.version, "v", false, "Print the version and exit")
+	flag.BoolVar(&args.help, "help", false, "Print this help message and exit")
+	flag.BoolVar(&args.help, "h", false, "Print this help message and exit")
+	flag.StringVar(&args.testdata, "testdata", "", "Capture a copy of all input and output to the specified directory. Useful for debugging or generating test data.")
+
+	flag.Parse()
+
+	// Handle special flags.
+
+	if args.version {
+		//nolint:forbidigo // use of fmt.Println
+		fmt.Println(Version)
+		return
+	}
+
+	if args.help {
+		flag.Usage()
+		return
+	}
+
+	// Configure logging.
+
+	log.SetFlags(log.Lshortfile)
+
+	// Configure input and output.
+
+	var reader io.Reader = os.Stdin
+	var writer io.Writer = os.Stdout
+
+	if args.testdata != "" {
+		if err := os.MkdirAll(args.testdata, 0o755); err != nil {
+			log.Fatal("Failed to create testdata directory: ", err)
+		}
+
+		inputFile, err := os.Create(args.testdata + "/input.jsonrpc")
+		if err != nil {
+			log.Fatal("Failed to create input file: ", err)
+		}
+		defer inputFile.Close()
+
+		outputFile, err := os.Create(args.testdata + "/output.jsonrpc")
+		if err != nil {
+			//nolint:gocritic // exitAfterDefer
+			log.Fatal("Failed to create output file: ", err)
+		}
+		defer outputFile.Close()
+
+		reader = io.TeeReader(reader, inputFile)
+		writer = io.MultiWriter(writer, outputFile)
+	}
+
+	// Run the LSP server.
 
 	server := &lsp.Server{
 		ServerInfo: types.ServerInfo{
 			Name:    "openapiv3-lsp",
-			Version: "0.1.0",
+			Version: Version,
 		},
-		Reader:  os.Stdin,
-		Writer:  os.Stdout,
+		Reader:  reader,
+		Writer:  writer,
 		Handler: &analysis.Handler{},
 	}
 
@@ -26,3 +89,33 @@ func main() {
 		log.Fatal("LSP server error: ", err)
 	}
 }
+
+// shadowReader wraps a primary reader and splits the input to a secondary
+// writer.
+type shadowReader struct {
+	reader io.Reader
+	shadow io.Writer
+}
+
+func (r shadowReader) Read(p []byte) (n int, err error) {
+	n, err = r.reader.Read(p)
+	_, _ = r.shadow.Write(p[:n])
+	return n, err
+}
+
+var _ io.Reader = shadowReader{}
+
+// shadowWriter wraps a primary writer and splits the output to a secondary
+// writer.
+type shadowWriter struct {
+	writer io.Writer
+	shadow io.Writer
+}
+
+func (w shadowWriter) Write(p []byte) (n int, err error) {
+	n, err = w.writer.Write(p)
+	_, _ = w.shadow.Write(p)
+	return n, err
+}
+
+var _ io.Writer = shadowWriter{}
---
+++ version.go
@@ -0,0 +1,4 @@
+package main
+
+// NOTE(asanyder): Version is set by release-please.
+const Version = "0.1.0"
