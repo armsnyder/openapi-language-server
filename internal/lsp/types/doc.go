e8369e127cc2a8f554e02717026fb133c6605d56
Adam Snyder <armsnyder@gmail.com>
2024-06-23 19:20:35 -0700

    chore: add types package doc

---
+++ internal/lsp/types/doc.go
@@ -0,0 +1,3 @@
+// Package types contains the Language Server Protocol specification types.
+// See https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/.
+package types
